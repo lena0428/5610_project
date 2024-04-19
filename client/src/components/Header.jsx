e661d738177e36791eb170fc2487112f995030de
lena0428 <139748662+lena0428@users.noreply.github.com>
2024-04-18 23:11:39 -0700

    fix code

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = ({ title }) => {
                     <FaUser /> Profile
                   </Nav.Link>
                   <Nav.Link
-                    onClick={() => logout({ returnTo: window.location.origin })}
+                    onClick={() => logout({ returnTo: process.env.REACT_APP_BASE_URL })}
                   >
                     <RiLogoutBoxRLine /> Log Out
                   </Nav.Link>
